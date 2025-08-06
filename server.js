0159c568c99aef95a5370daa7eab4bc69261a517
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-08-06 14:23:19 -0400

    add boiler plate serverJS init file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(JSON.stringify("Checking endpoint"));
+});
+
+const startServer = () => {
+  try {
+    app.listen(3000, () => {
+      console.log("Server is running on http://localhost:3000");
+    });
+  } catch (error) {
+    console.error("Failed to start server", error);
+    process.exit(1);
+  }
+};
+
+startServer();
